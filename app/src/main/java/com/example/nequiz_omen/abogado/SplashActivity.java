8c3894e119945458776b499f6e0646e37f8f04f2
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-06-13 23:19:07 -0500

    version 1.0  estable
    se sube proyecto estable  y compatible de   lollipop  hasta la version OREO

---
+++ app/src/main/java/com/example/nequiz_omen/abogado/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.nequiz_omen.abogado;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Nequiz_OMEN on 10/06/2018.
+ */
+
+public class SplashActivity extends Activity {
+
+
+
+}

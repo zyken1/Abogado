98c30350d2efa9fc53c7bea2639419762b34ae85
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-06-18 17:54:26 -0500

    new spinner
    se mete nuevo spinner listo para configurarlo

--- app/src/main/java/com/example/nequiz_omen/abogado/Jucios_Formulario.java
+++ app/src/main/java/com/example/nequiz_omen/abogado/Jucios_Formulario.java
@@ -6,10 +6,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class Jucios_Formulario extends AppCompatActivity {
+public class Jucios_Formulario extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    //Declaración de variables
+    private Spinner spinnerPro, spinnerLoc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,18 +36,49 @@ public class Jucios_Formulario extends AppCompatActivity {
             }
         });
 
+        //Referenciado de variables del XML
+        spinnerPro = (Spinner) findViewById(R.id.spinner_Juicio);
+        spinnerLoc = (Spinner) findViewById(R.id.spinner_Etapa);
+        //Construcción del "adaptador" para el primer Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.array_provincias, //Se carga el array definido en el XML
+                android.R.layout.simple_spinner_item);
 
-        //SE CREA Y SE MANDA A LLAMAR EL SPINNER  TIPO DE JUICIO
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_Juicio);
-        String[] tipo_juicio = {"Selecciona el Tipo de Expediente","JUICIO ORDINARIO CIVIL","JUICIO ORDINARIO MERCANTIL","JUICIO ORAL MERCANTIL","JUICIO EJECUTIVO MERCANTIL ORAL","JUICIO EJECUTIVO MERCANTIL","JUICIO ORDINARIO PENAL","AMPARO INDIRECTO","AMPARO DIRECTO","INCIDENTE DE SUSPENSIÓN","JUICIO ORDINARIO LABORAL"};
-        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tipo_juicio));
+        //Se carga el tipo de vista para el adaptador
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        //SE CREA Y SE MANDA A LLAMAR EL SPINNER  ETAPA  PROCESAL
-        Spinner spinner1 = (Spinner) findViewById(R.id.spinner_Etapa);
-        String[] etapa_procesal = {"Selecciona la Etapa del proceso", "A","B","C","D","E"};
-        spinner1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, etapa_procesal));
+        //Se aplica el adaptador al Spinner de localidades
+        spinnerPro.setAdapter(adapter);
+        //Se aplica listener para saber que item ha sido seleccionado
+        //y poder usarlo en el método "onItemSelected"
+        spinnerPro.setOnItemSelectedListener(this);
 
     }//end ON CREATE
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //Se guarda en array de enteros los arrays de las provincias
+        int[] localidades = {R.array.array_sevilla,R.array.array_malaga};
+
+        //Construcción del "adaptador" para el segundo Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                localidades[position],//En función de la provincia, se carga el array que corresponda del XML
+                android.R.layout.simple_spinner_item);
+
+        //Se carga el tipo de vista para el adaptadori
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Se aplica el adaptador al Spinner de localidades
+        spinnerLoc.setAdapter(adapter);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
